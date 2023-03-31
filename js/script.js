037cb237ca73a0f95fec8cedea25372daddf3603
GiacomoArena <giacomoarena28@gmail.com>
2023-03-31 17:14:39 +0200

    fixing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//variables
 const output = document.querySelector('#num');
 const button = document.querySelector('button');
 const result = document.getElementById('result')
@@ -6,9 +7,11 @@ let randomNum ;
 let popUpMsg;
 let arrayRandomNum = []
 let arrayNum = []
-
+//play button
 button.addEventListener('click', function(){
+  //reset h1
   result.innerHTML = "";
+  //start numbers 
   for (let i = 0; i < 5; i++) {
     randomNum = uniqueRandomNum(blackList, 1, 100).toString()
     arrayRandomNum.push(randomNum)
@@ -20,11 +23,12 @@ button.addEventListener('click', function(){
     }
     
     
-    
+    //prompt
     setTimeout(function() {
       popUpMsg = prompt("Inserisci i numeri suddivisi da uno spazio")
       arrayNum = popUpMsg.split(' ');
       let counter = 0
+      
       for (let i = 0; i < arrayNum.length; i++) {
         
         if (arrayRandomNum.includes(arrayNum[i])) {
@@ -33,18 +37,12 @@ button.addEventListener('click', function(){
       }
       if (counter === 5 ) {
         result.innerHTML = `Bravo hai indovinato tutti i numeri, sei fortissimo !1!1`; 
-
-        console.log(`Bravo hai indovinato tutti i numeri, sei fortissimo !1!1`);
       }
       else if(counter === 0){
         result.innerHTML =`Mi spiace ma non E andata bene, 0 su 5`
-
-        console.log(`Mi spiace ma non E andata bene, 0 su 5`);
       }
       else{
-        result.innerHTML =`bravo hai indovinato ${counter} su 5 numeri `
-
-        console.log(`bravo hai indovinato ${counter} su 5 numeri `);
+        result.innerHTML =`Bravo hai indovinato ${counter} su 5 numeri `
       }
     
     console.log(arrayNum);
@@ -52,7 +50,7 @@ button.addEventListener('click', function(){
 
 })
 
-
+//random num function
 
 function uniqueRandomNum(blackList, min, max) {
   let randomNumber = Math.floor(Math.random() * (max - min +1)+min);
